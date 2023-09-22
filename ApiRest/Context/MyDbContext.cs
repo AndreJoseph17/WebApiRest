af0639f654feb7f9a3c8181af629d0f3079e53dd
Andre Jaramillo <andre.1192@hotmail.com>
2023-09-21 23:55:43 -0500

    Agregar archivos de proyecto.

---
+++ ApiRest/Context/MyDbContext.cs
@@ -0,0 +1,12 @@
+﻿using ApiRest.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiRest.Context
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) { }
+
+        public DbSet<Producto> Productos { get; set; }
+    }
+}
